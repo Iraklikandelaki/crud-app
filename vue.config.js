7fca87d25424a7053fdd0512440a81dd0f848ce7
i_kandelaki <kandelakiirakli01@gmail.com>
2022-03-02 13:17:21 +0400

    add deployement script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 module.exports = {
+	publicPath: process.env.NODE_ENV === "production" ? "/crud-app" : "/",
 	css: {
+		extract: false,
 		loaderOptions: {
 			sass: {
 				additionalData: bootstrapSassAbstractsImports.join('\n')
